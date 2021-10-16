b2aa4ac1ef6d56c74ed1e06a73a5070b1ca1df33
Daniella Caden <daniella.wwcd@gmail.com>
2021-10-16 19:39:03 +0300

    add last

--- try.js
+++ try.js
@@ -23,19 +23,19 @@ heading.textContent="My first div";
  let content= document.querySelector('div.container');
 
 
-let newCard= document.createElement('div');
+let newC= document.createElement('div');
 
-let heading= document.createElement('h3');
-heading.textContent="My first div";
+let head= document.createElement('h3');
+head.textContent="My Second div";
 
- let paragraph=document.createElement('p');
- paragraph.textContent= "we are creating a paragraph";
+ let paragraph2=document.createElement('p');
+ paragraph2.textContent= "we are creating a second paragraph";
 
- let picture=document.createElement('img');
- picture.src="Flower.jpg";
+ let image=document.createElement('img');
+ image.src="Rose.jpg";
 
- newCard.appendChild(heading);
- newCard.appendChild(paragraph);
- newCard.appendChild(picture);
+ newC.appendChild(head);
+ newC.appendChild(paragraph2);
+ newC.appendChild(image);
 
- container.appendChild(newCard);
+ content.appendChild(newC);
