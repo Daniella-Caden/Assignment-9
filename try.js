3620927e2f24801fba35d406c186b713c48c32fc
Daniella Caden <daniella.wwcd@gmail.com>
2021-10-16 18:41:11 +0300

    add 8

--- try.js
+++ try.js
@@ -1,6 +1,8 @@
-let newCard= document.querySelector('div.container');
+let newCard= document.createElement('div');
+
 let heading= document.createElement('h3');
 heading.textContent="My first div";
+
  let paragraph=document.createElement('p');
  paragraph.textContent= "we are creating";
 
