86807c1da9fcbc2399d12f4018d8c355a9572745
Daniella Caden <daniella.wwcd@gmail.com>
2021-10-16 19:34:26 +0300

    add 7th

--- try.js
+++ try.js
@@ -7,9 +7,35 @@ let heading= document.createElement('h3');
 heading.textContent="My first div";
 
  let paragraph=document.createElement('p');
- paragraph.textContent= "we are creating";
+ paragraph.textContent= "we are creating a paragraph";
+
+ let picture=document.createElement('img');
+ picture.src="Flower.jpg";
 
  newCard.appendChild(heading);
  newCard.appendChild(paragraph);
+ newCard.appendChild(picture);
+
+ container.appendChild(newCard);
+
+
+
+ let content= document.querySelector('div.container');
+
+
+let newCard= document.createElement('div');
+
+let heading= document.createElement('h3');
+heading.textContent="My first div";
+
+ let paragraph=document.createElement('p');
+ paragraph.textContent= "we are creating a paragraph";
+
+ let picture=document.createElement('img');
+ picture.src="Flower.jpg";
+
+ newCard.appendChild(heading);
+ newCard.appendChild(paragraph);
+ newCard.appendChild(picture);
 
  container.appendChild(newCard);
